8025cf58445c23d3c5de50b57a26e8acd0e490a2
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-02-02 16:03:11 +0000

    Initial project setup

---
+++ flag-viewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flag-viewer
+//
+//  Created by Bradley Chesworth on 02/02/2020.
+//  Copyright © 2020 Brad Chesworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
