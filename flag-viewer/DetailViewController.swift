2d5b38d36dede3243831a7f9036023f4ca0b38ac
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-02-02 22:06:41 +0000

    Added sharing functionality for the images

--- flag-viewer/DetailViewController.swift
+++ flag-viewer/DetailViewController.swift
@@ -19,8 +19,23 @@ class DetailViewController: UIViewController {
         title = selectedCountry
         navigationItem.largeTitleDisplayMode = .never
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         if let countryToLoad = selectedCountry {
             imageView.image = UIImage(named: countryToLoad)
         }
+    }
+    
+    @objc func shareTapped() {
+        guard let country = imageView.image?.jpegData(compressionQuality: 0.8) else {
+             print("No image found")
+             return
+         }
+         
+         let vc = UIActivityViewController(activityItems: [country], applicationActivities: [])
+         
+         vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+         
+         present(vc, animated: true)
     }
 }
